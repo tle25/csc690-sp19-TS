5b7482632cf8efeff59997933b863636251098a2
tle25 <tle25@mail.sfsu.edu>
2019-04-21 21:39:51 -0700

    created UI view

--- MemoryPuzzle/MemoryPuzzle/ViewController.swift
+++ MemoryPuzzle/MemoryPuzzle/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var gameView: UIView!
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    @IBOutlet weak var GameStatusLabel: UILabel!
+    
+    
+    
+    @IBAction func resetAction(_ sender: Any) {
+    }
+    
+    
+    @IBAction func gameModeAction(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
