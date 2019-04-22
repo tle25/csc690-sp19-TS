54bad862a5793733d14139bccfc27faa3d67b3ef
tle25 <tle25@mail.sfsu.edu>
2019-04-21 23:03:57 -0700

    added grid numberic cards

--- MemoryPuzzle/MemoryPuzzle/ViewController.swift
+++ MemoryPuzzle/MemoryPuzzle/ViewController.swift
@@ -13,11 +13,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var gameView: UIView!
-    
-    
     @IBOutlet weak var timeLabel: UILabel!
-    
-    
     @IBOutlet weak var GameStatusLabel: UILabel!
     
     
@@ -29,9 +25,57 @@ class ViewController: UIViewController {
     @IBAction func gameModeAction(_ sender: Any) {
     }
     
+
+    var gameMode : Int = 4
+    var tileSize: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeTiles()
+    }
+    
+    func makeTiles(){
+        tileSize = gameView.frame.size.width / CGFloat(gameMode)
+        
+        
+//       tileSize - 5 for the gap between each card
+        let tileCgSize = CGSize(width: tileSize - 5, height: tileSize - 5)
+        
+        
+        var xCenter = tileSize / 2.0
+        var yCenter = tileSize / 2.0
+        var counter = 0
+        
+        //        for v in 0..<gameMode{
+        let row = gameMode * 3 / 2
+        let col = gameMode
+        for _ in 0..<row{
+            for _ in 0..<col {
+                
+                
+                let tile = UILabel (frame: CGRect(origin: CGPoint.zero, size: tileCgSize))
+            
+                tile.font = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.bold)
+                
+                let tileCenter = CGPoint(x: xCenter, y: yCenter)
+                
+                if counter == row * col / 2{
+                    counter = 0
+                }
+                
+                tile.text = "\(counter)"
+                counter += 1
+                
+                tile.center = tileCenter
+                xCenter += tileSize
+                
+                tile.backgroundColor = UIColor.darkGray
+                gameView.addSubview(tile)
+            }
+            xCenter = tileSize/2
+            yCenter += tileSize
+        }
+        
     }
 
 
