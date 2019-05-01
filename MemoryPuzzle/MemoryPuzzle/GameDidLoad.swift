973bf27bf4ef49b1ac12620b86b8f9351f077fff
tle25 <tle25@mail.sfsu.edu>
2019-04-30 21:15:32 -0700

    add reset feature

--- MemoryPuzzle/MemoryPuzzle/GameDidLoad.swift
+++ MemoryPuzzle/MemoryPuzzle/GameDidLoad.swift
@@ -3,21 +3,8 @@ import UIKit
 extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeTiles()
-        randomize()
         
-        if(gameTimer != nil){
-            gameTimer.invalidate()
-            timeLabel.text = "00\' : 00\""
-        }
-        
-        //set time interver call every 1 second, or minisec = 1/60
-        gameTimer = Timer.scheduledTimer(timeInterval: 1,
-                                         target: self,
-                                         selector: #selector(timerFunction),
-                                         userInfo: nil,
-                                         repeats: true)
-        
+        self.resetAction(self)
     }
     
     }
--- MemoryPuzzle/MemoryPuzzle/Reset.swift
+++ MemoryPuzzle/MemoryPuzzle/Reset.swift
@@ -3,8 +3,26 @@ import UIKit
 extension ViewController{
 
     @IBAction func resetAction(_ sender: Any) {
+        TimeCounting = 0
+        
+        for any in tilesArr {
+            any.removeFromSuperview()
+        }
         
         
+        makeTiles()
+        randomize()
+        
+        if(gameTimer != nil){
+            gameTimer.invalidate()
+            timeLabel.text = "00\' : 00\""
+        }
+        //set time interver call every 1 second, or minisec = 1/60
+        gameTimer = Timer.scheduledTimer(timeInterval: 1,
+                                         target: self,
+                                         selector: #selector(timerFunction),
+                                         userInfo: nil,
+                                         repeats: true)
         
     }
     
