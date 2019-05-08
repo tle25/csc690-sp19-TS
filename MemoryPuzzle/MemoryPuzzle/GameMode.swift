1798435bbfde3f4f096605dddda2d36dd06fc14e
tle25 <tle25@mail.sfsu.edu>
2019-05-07 21:44:17 -0700

    added game mode

--- MemoryPuzzle/MemoryPuzzle/GameMode.swift
+++ MemoryPuzzle/MemoryPuzzle/GameMode.swift
@@ -3,7 +3,25 @@ import UIKit
 extension ViewController{
     
     @IBAction func gameModeAction(_ sender: Any) {
-        
+        if let mySegCtrl = sender as? UISegmentedControl {
+            let index = mySegCtrl.selectedSegmentIndex
+            
+            switch (index){
+            case 0:
+                gameMode = 2
+            case 1:
+                gameMode = 4
+            case 2:
+                gameMode = 6
+            case 3:
+                gameMode = 8
+                
+            default:
+                break
+            }
+            
+            resetAction(self)
+        }
         
     }
 }
